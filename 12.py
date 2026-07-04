b9fc8dde41b53f4fdd798b9f96a00cdd72ad9d6c
DanielJack234 <jackubongdaniel@gmail.com>
2026-07-04 16:32:28 +0100

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,13 @@
+class student:
+      grade = 10
+      name = "Penguin"
+
+      def introduction(self):
+          print("Hi, I am a student")
+
+      def details(self):
+          print("My name is", self.name)
+          print("I am in grade", self.grade)
+ob = student()
+ob.introduction()
+ob.details()
